1b575939cc5ab27a5e550c70465b6b2df0308586
abg3000 <abg3000@naver.com>
2022-02-18 17:15:12 +0900

    [Feat] 최근 검색어 기능 구현
    - 검색어 최신순으로 5개만 노출 되도록 구현

--- src/components/Search/ItemList.tsx
+++ src/components/Search/ItemList.tsx
@@ -1,11 +1,31 @@
+import { useEffect } from 'react';
 import { dbArr } from 'types/db';
-
+import Searched from './Searched';
+import MatchItem from './MatchItem';
 interface Props {
   inputFocused: boolean;
   searched: string[];
   matches: dbArr;
 }
 function ItemList({ inputFocused, searched, matches }: Props) {
-  return <div className={'item-list' + (inputFocused ? ' active' : '')}></div>;
+  useEffect(() => {
+    console.log(matches);
+  }, []);
+  return (
+    <div className={'item-list' + (inputFocused ? ' active' : '')}>
+      <p>최근 검색어</p>
+      <ul>
+        {searched.map((item) => {
+          return <Searched key={item} searchedName={item} />;
+        })}
+      </ul>
+      <p>일치하는 제품</p>
+      <ul>
+        {matches.map((item) => {
+          return <MatchItem key={item.id} matchItem={item} />;
+        })}
+      </ul>
+    </div>
+  );
 }
 export default ItemList;
